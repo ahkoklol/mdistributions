17cb6400564c04f4650db8b64b2ddc14e8530426
wayne <wayne.wan-chow-wah@etu.umontpellier.fr>
2025-11-16 17:39:26 +0400

    Could not fix zio.trace

--- back/modules/server/src/main/scala/com/example/back/http/controllers/EmailController.scala
+++ back/modules/server/src/main/scala/com/example/back/http/controllers/EmailController.scala
@@ -18,35 +18,35 @@ import io.scalaland.chimney.dsl.*
 class EmailController private (emailService: EmailService, jwtService: JWTService)
     extends SecuredBaseController[String, UserID](jwtService.verifyToken) {
 
-  val createEmail: ServerEndpoint[Any, Task] = EmailEndpoint.createEmail.zServerAuthenticatedLogic { case (user: UserID, emailDto: Email) =>
-    val emailEntity = EmailEntity(
+  val createEmail: ServerEndpoint[Any, Task] =
+    EmailEndpoint.createEmail.zServerAuthenticatedLogic { (user: UserID, emailDto: Email) =>
+      val trace = summon[zio.Trace]
+      val emailEntity = EmailEntity(
         id = 0,
         userId = user.id,
         subject = emailDto.subject,
         body = emailDto.body,
         googleSheetsLink = emailDto.googleSheetsLink,
         creationDate = ZonedDateTime.now()
-    )
-    emailService.create(emailEntity).map(_.into[Email].transform)
+      )
+      emailService.create(emailEntity).map(_.into[Email].transform)
     }
 
   val getEmailById: ServerEndpoint[Any, Task] =
-  EmailEndpoint.getEmailById.zServerAuthenticatedLogic { case (userId: UserID, emailId: Long) =>
-    emailService
-      .getById(emailId)
-      .map(_.map(_.into[Email].transform))
-  }
-
+    EmailEndpoint.getEmailById.zServerAuthenticatedLogic { (user: UserID, emailId: Long) =>
+      val trace = summon[zio.Trace]
+      emailService.getById(emailId).map(_.map(_.into[Email].transform))
+    }
 
   val getEmails: ServerEndpoint[Any, Task] =
-  EmailEndpoint.getEmails.zServerAuthenticatedLogic { (user: UserID) =>
-    emailService
-      .getByCreationDateDesc()
-      .map(_.filter(_.userId == user.id).map(_.into[Email].transform))
-  }
+    EmailEndpoint.getEmails.zServerAuthenticatedLogic { (user: UserID) =>
+      val trace = summon[zio.Trace]
+      emailService.getByCreationDateDesc()
+        .map(_.filter(_.userId == user.id).map(_.into[Email].transform))
+    }
 
-
-  override val routes: List[ServerEndpoint[Any, Task]] = List(createEmail, getEmailById, getEmails)
+  override val routes: List[ServerEndpoint[Any, Task]] =
+    List(createEmail, getEmailById, getEmails)
 }
 
 object EmailController {
